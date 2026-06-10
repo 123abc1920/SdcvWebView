5aa3e8ab2ba74c12dbfbba5ca5a1e2d3884cb791
123abc1920 <123abc1920@mail.ru>
2026-06-10 22:57:10 +0300

    build: configure vue

---
+++ frontend/components/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-5">
+    <input v-model="word" class="form-control" placeholder="Введите слово" />
+    <button @click="translate" class="btn btn-primary mt-2">Перевести</button>
+    <pre class="mt-3">{{ result }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const word = ref("");
+const result = ref("");
+
+const translate = () => {
+  result.value = `Вы ввели: ${word.value}`;
+};
+</script>
