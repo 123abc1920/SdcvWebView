e569db2cc86fad6fdc9eddeca0778d018151b152
123abc1920 <123abc1920@mail.ru>
2026-06-18 14:01:14 +0300

    feat(frontend/components/main): fix footer

--- frontend/components/main.vue
+++ frontend/components/main.vue
@@ -1,29 +1,31 @@
 <template>
-  <header>
-    <nav class="navbar navbar-expand bg-dark text-light">
-      <div class="container-fluid">
-        <a class="navbar-brand text-light fs-2 fw-bold" href="#">App Name</a>
+  <div class="d-flex flex-column min-vh-100">
+    <header>
+      <nav class="navbar navbar-expand bg-dark text-light">
+        <div class="container-fluid">
+          <a class="navbar-brand text-light fs-2 fw-bold" href="#">App Name</a>
+        </div>
+      </nav>
+    </header>
+
+    <main class="mt-4 mb-4 flex-grow-1">
+      <div class="container" style="max-width: 1000px; width: 100%">
+        <textarea
+          class="form-control fs-5"
+          placeholder="Введите слово..."
+          rows="6"
+        ></textarea>
+
+        <p class="fs-5 mt-3 p-3 rounded">Результат</p>
       </div>
-    </nav>
-  </header>
+    </main>
 
-  <main class="mt-4 mb-4">
-    <div class="container" style="max-width: 1000px; width: 100%">
-      <textarea
-        class="form-control fs-5"
-        placeholder="Введите слово..."
-        rows="6"
-      ></textarea>
-
-      <p class="fs-5 mt-3 p-3 rounded">Результат</p>
-    </div>
-  </main>
-
-  <footer class="bg-dark text-light py-3">
-    <div class="container-fluid">
-      <p class="fs-5 text-center mb-0">2026</p>
-    </div>
-  </footer>
+    <footer class="bg-dark text-light py-3 mt-auto">
+      <div class="container-fluid">
+        <p class="fs-5 text-center mb-0">2026</p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script setup>
