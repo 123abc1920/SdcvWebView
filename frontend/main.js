c6a53e260f61790febcdd032eff3996fa46e4c8c
123abc1920 <123abc1920@mail.ru>
2026-06-18 13:57:33 +0300

    feat(frontend/components/main): improve adaptivity

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import Translate from './components/main.vue'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootswatch/dist/lux/bootstrap.min.css'
 
 createApp(Translate).mount('#app')
