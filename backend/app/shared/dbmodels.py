31091db249883c1407d26404395fd44ecc9d7b88
123abc1920 <123abc1920@mail.ru>
2026-06-22 15:32:58 +0300

    build(backend): add autoincrement ids

--- backend/app/shared/dbmodels.py
+++ backend/app/shared/dbmodels.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Translation(db.Model):
     __tablename__ = "translations"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     word = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"))
@@ -14,7 +14,7 @@ class Translation(db.Model):
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text, unique=True)
     password_hash = db.Column(db.Text)
     is_admin = db.Column(db.Boolean, default=False)
